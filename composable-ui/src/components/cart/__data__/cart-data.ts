7fc7458a3c1dea95f94e481c5994ad13be94c2eb
Marcin Slezak <marcin.slezak@voucherify.io>
2023-11-02 16:58:58 +0100

    fix build error

--- composable-ui/src/components/cart/__data__/cart-data.ts
+++ composable-ui/src/components/cart/__data__/cart-data.ts
@@ -1,10 +1,14 @@
+import { CartItemWithDiscounts } from '@composable/types'
 import { CartData } from '../../../hooks'
 
 export const cartData: CartData = {
   id: '7a6dd462-24dc-11ed-861d-0242ac120002',
+  cartType: 'CartWithDiscounts',
+  redeemables: [],
   items: [
     {
       id: '1',
+      cartItemType: 'CartItemWithDiscounts',
       category: 'Accessories',
       type: 'Bag',
       name: 'Venture Daypack',
@@ -17,12 +21,18 @@ export const cartData: CartData = {
       sku: 'SKU-A1-2345',
       slug: 'venture-daypack',
       quantity: 1,
+      discounts: {
+        subtotalAmount: '',
+      },
     },
-  ],
+  ] as CartItemWithDiscounts[],
   summary: {
     taxes: '2.45',
     totalPrice: '35.00',
     shipping: 'Free',
+    discountAmount: '0',
+    totalDiscountAmount: '0',
+    grandPrice: '0',
   },
   isLoading: false,
   isEmpty: false,
