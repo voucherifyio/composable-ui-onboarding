c025d7a960af49f04510f754a4581582893b3a32
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-12-05 09:01:39 +0100

    Put file operations in operations.ts file

--- scripts/src/voucherify-setup/install.ts
+++ scripts/src/voucherify-setup/install.ts
@@ -1,96 +1,8 @@
-import * as fs from 'fs/promises'
-import * as path from 'path'
-
-// Functions
-export async function replaceInFile(
-  filePath: string,
-  searchPhrase: string,
-  replacePhrase: string
-): Promise<void> {
-  try {
-    // Read the content of the file
-    const fullPath = path.join(__dirname, filePath) // assuming file paths are relative to the script's directory
-    const data = await fs.readFile(fullPath, 'utf8')
-
-    // Replace the search phrase with the replacement phrase
-    const updatedContent = data.replace(
-      new RegExp(searchPhrase, 'g'),
-      replacePhrase
-    )
-
-    // Write the updated content back to the file
-    await fs.writeFile(fullPath, updatedContent, 'utf8')
-
-    console.log(`Replacement complete in ${fullPath}`)
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
-
-export async function processFiles(
-  filePaths: string[],
-  searchPhrase: string,
-  replacePhrase: string
-): Promise<void> {
-  try {
-    for (const filePath of filePaths) {
-      await replaceInFile(filePath, searchPhrase, replacePhrase)
-    }
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
-
-async function addVoucherifyImplementationToCreateOrderFile(
-  createOrderFilePath: string,
-  importContent: string,
-  updatePaidOrderContent: string,
-  searchedText: string
-) {
-  try {
-    const fullPath = path.join(__dirname, createOrderFilePath)
-    const data = await fs.readFile(fullPath, 'utf8')
-    const dataWithImportAdded = importContent.concat(data)
-
-    const updatedContent = dataWithImportAdded.replace(
-      searchedText,
-      (match) => {
-        return match + updatePaidOrderContent
-      }
-    )
-
-    await fs.writeFile(fullPath, updatedContent, 'utf8')
-    console.log(`Replacement complete in ${fullPath}`)
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
-
-async function updatePackageJson(
-  packageJsonPath: string,
-  newDependency: string,
-  newDependencyVersion: string
-): Promise<void> {
-  try {
-    // Load the package.json file
-    const fullPath = path.join(__dirname, packageJsonPath)
-    const packageJsonContent = await fs.readFile(fullPath, 'utf8')
-    const packageJson = JSON.parse(packageJsonContent)
-
-    // Add the new dependency
-    if (!packageJson.dependencies) {
-      packageJson.dependencies = {}
-    }
-    packageJson.dependencies[newDependency] = newDependencyVersion // Replace 'newDependency' with the actual package name
-
-    // Save the updated package.json file
-    await fs.writeFile(fullPath, JSON.stringify(packageJson, null, 2), 'utf8')
-
-    console.log(`Dependency added to package.json: ${newDependency}`)
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
+import {
+  addVoucherifyToCreateOrderFile,
+  processFiles,
+  updatePackageJson,
+} from './operations'
 
 // Usage
 // Replace classic usage with Voucherify usage
@@ -120,7 +32,7 @@ const updatePaidOrderContent =
 const searchedText =
   'const updatedOrder = generateOrderFromCart(cart, checkout)'
 
-addVoucherifyImplementationToCreateOrderFile(
+addVoucherifyToCreateOrderFile(
   createOrderFilePath,
   importContent,
   updatePaidOrderContent,
--- scripts/src/voucherify-setup/uninstall.ts
+++ scripts/src/voucherify-setup/uninstall.ts
@@ -1,59 +1,10 @@
-import * as fs from 'fs/promises'
-import * as path from 'path'
-import { processFiles } from './install'
-
-async function removeVoucherifyImplementationFromCreateOrderFile(
-  createOrderFilePath: string,
-  importContent: string,
-  updatePaidOrderContent: string
-) {
-  try {
-    const fullPath = path.join(__dirname, createOrderFilePath)
-    const data = await fs.readFile(fullPath, 'utf8')
-    const dataWithImportRemoved = data.replace(importContent, '')
-
-    const updatedContent = dataWithImportRemoved.replace(
-      updatePaidOrderContent,
-      ''
-    )
-    await fs.writeFile(fullPath, updatedContent, 'utf8')
-    console.log(`Replacement complete in ${fullPath}`)
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
-
-async function removeFromPackageJson(
-  packageJsonPath: string,
-  dependency: string
-): Promise<void> {
-  try {
-    // Load the package.json file
-    const fullPath = path.join(__dirname, packageJsonPath)
-    const packageJsonContent = await fs.readFile(fullPath, 'utf8')
-    const packageJson = JSON.parse(packageJsonContent)
-
-    // Add the new dependency
-    if (!packageJson.dependencies) {
-      packageJson.dependencies = {}
-    }
-    packageJson.dependencies = Object.fromEntries(
-      Object.entries(packageJson.dependencies).filter(
-        ([key, val]) => key !== dependency
-      )
-    )
-
-    // Save the updated package.json file
-    await fs.writeFile(fullPath, JSON.stringify(packageJson, null, 2), 'utf8')
-
-    console.log(`Dependency removed from package.json: ${dependency}`)
-  } catch (err) {
-    console.error(`Error: ${err}`)
-  }
-}
+import {
+  processFiles,
+  removeFromPackageJson,
+  removeVoucherifyFromCreateOrderFile,
+} from './operations'
 
 // Usage
-
 // Replace Voucherify usage with classic usage
 const filePaths = [
   '../../../packages/commerce-generic/src/services/cart/add-cart-item.ts',
@@ -63,7 +14,6 @@ const filePaths = [
   '../../../packages/commerce-generic/src/services/cart/get-cart.ts',
   '../../../packages/commerce-generic/src/services/cart/delete-voucher.ts',
   '../../../packages/commerce-generic/src/services/cart/add-voucher.ts',
-  '../../../packages/commerce-generic/src/services/checkout/create-order.ts',
 ]
 
 const replacePhrase = "from './discount'"
@@ -79,10 +29,8 @@ const updatePaidOrderContent =
   '  \n  // V%\n' +
   "  updatedOrder.payment = 'paid'\n" +
   '  await orderPaid(updatedOrder)'
-const searchedText =
-  'const updatedOrder = generateOrderFromCart(cart, checkout)'
 
-removeVoucherifyImplementationFromCreateOrderFile(
+removeVoucherifyFromCreateOrderFile(
   createOrderFilePath,
   importContent,
   updatePaidOrderContent
