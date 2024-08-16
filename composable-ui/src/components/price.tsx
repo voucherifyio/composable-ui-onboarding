14009bad53aedae1aea1edc629f9d996a949590b
p-zielinski <piotrzielinski96@yahoo.com>
2024-08-16 13:29:53 +0200

    Fix NaN price

--- composable-ui/src/components/price.tsx
+++ composable-ui/src/components/price.tsx
@@ -4,7 +4,7 @@ import { APP_CONFIG } from 'utils/constants'
 
 export interface PriceProps {
   rootProps?: BoxProps
-  price?: number
+  price?: number | string
 }
 
 export const Price = ({ rootProps, price }: PriceProps) => {
@@ -14,11 +14,25 @@ export const Price = ({ rootProps, price }: PriceProps) => {
     return null
   }
 
+  if (typeof price === 'number') {
+    return (
+      <Box {...rootProps}>
+        {Number.isNaN(price)
+          ? price
+          : intl.formatNumber(price, {
+              currency: APP_CONFIG.CURRENCY_CODE,
+              style: 'currency',
+            })}
+      </Box>
+    )
+  }
+
+  const value = parseFloat(price)
   return (
     <Box {...rootProps}>
-      {Number.isNaN(price)
+      {Number.isNaN(value)
         ? price
-        : intl.formatNumber(price, {
+        : intl.formatNumber(value, {
             currency: APP_CONFIG.CURRENCY_CODE,
             style: 'currency',
           })}
