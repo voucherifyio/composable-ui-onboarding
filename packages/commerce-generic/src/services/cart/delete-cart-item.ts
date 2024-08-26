7b347c0506e8455b201c962786c67ca06821562c
p-zielinski <piotrzielinski96@yahoo.com>
2024-08-26 10:43:27 +0200

    fix error

--- packages/commerce-generic/src/services/cart/delete-cart-item.ts
+++ packages/commerce-generic/src/services/cart/delete-cart-item.ts
@@ -1,7 +1,10 @@
 import { CommerceService } from '@composable/types'
 import { getCart, saveCart } from '../../data/mock-storage'
 
-import { calculateCartSummary } from '../../data/generate-cart-data'
+import {
+  calculateCartSummary,
+  generateEmptyCart,
+} from '../../data/generate-cart-data'
 import { updateCartDiscount } from '@composable/voucherify'
 
 export const deleteCartItem: CommerceService['deleteCartItem'] = async ({
@@ -11,13 +14,7 @@ export const deleteCartItem: CommerceService['deleteCartItem'] = async ({
   channel,
   dontApplyCodes,
 }) => {
-  const cart = await getCart(cartId)
-
-  if (!cart) {
-    throw new Error(
-      `[deleteCartItem] Could not found cart with requested cart id: ${cartId}`
-    )
-  }
+  const cart = (await getCart(cartId)) || generateEmptyCart(cartId)
 
   cart.items = cart.items.filter((item) => item.id !== productId)
 
--- packages/commerce-generic/src/services/cart/update-cart-item.ts
+++ packages/commerce-generic/src/services/cart/update-cart-item.ts
@@ -1,7 +1,10 @@
 import { CommerceService } from '@composable/types'
 import { getCart, saveCart } from '../../data/mock-storage'
 
-import { calculateCartSummary } from '../../data/generate-cart-data'
+import {
+  calculateCartSummary,
+  generateEmptyCart,
+} from '../../data/generate-cart-data'
 import { updateCartDiscount } from '@composable/voucherify'
 
 export const updateCartItem: CommerceService['updateCartItem'] = async ({
@@ -12,15 +15,7 @@ export const updateCartItem: CommerceService['updateCartItem'] = async ({
   channel,
   dontApplyCodes,
 }) => {
-  let cart = await getCart(cartId)
-
-  if (!cart) {
-    throw new Error(
-      `[updateCartItem] Could not found cart with requested cart id: ${cartId}`
-    )
-  }
-
-  const cartItem = cart.items.find((item) => item.id === productId)
+  let cart = (await getCart(cartId)) || generateEmptyCart(cartId)
 
   cart.items = cart.items
     .map((item) => (item.id === productId ? { ...item, quantity } : item))
